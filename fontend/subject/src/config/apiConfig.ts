c2598361e69b2daa1aae59f662b23f11b208c281
ngocthao <21130542@st.hcmuaf.edu.vn>
2025-05-25 00:13:11 +0700

    add env

--- fontend/subject/src/config/apiConfig.ts
+++ fontend/subject/src/config/apiConfig.ts
@@ -1,5 +1,5 @@
 // src/config/apiConfig.js
-const BASE_URL = "http://localhost:8080";
+const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:8080";
 const API_URL = `${BASE_URL}/api`;
 const AUTH_URL = `${BASE_URL}/auth`;
 const HANDLE_URL = `${BASE_URL}/handle`;
