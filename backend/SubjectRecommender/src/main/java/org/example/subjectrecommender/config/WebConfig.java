9c26dd91bd02be895c762634dddbc79d09c7b4a2
ngocthao <21130542@st.hcmuaf.edu.vn>
2025-06-01 13:14:48 +0700

    them de cuong chi tiet

--- backend/SubjectRecommender/src/main/java/org/example/subjectrecommender/config/WebConfig.java
+++ backend/SubjectRecommender/src/main/java/org/example/subjectrecommender/config/WebConfig.java
@@ -14,7 +14,7 @@
                 @Override
                 public void addCorsMappings(CorsRegistry registry) {
                     registry.addMapping("/**")
-                            .allowedOrigins("https://6831ff354451b20008c68ff2--enti-vt.netlify.app")  // địa chỉ React
+                            .allowedOrigins("http://localhost:5173")  // địa chỉ React
                             .allowedMethods("GET", "POST", "PUT", "DELETE")
                             .allowCredentials(true);
                 }
